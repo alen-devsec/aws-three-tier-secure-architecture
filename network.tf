9569b847e3f849eba1b8017be3b26460ad8e67a8
Alen-DevSecOps <ya5256961@gmail.com>
2026-02-19 15:56:59 +0400

    Create network.tf

---
+++ network.tf
@@ -0,0 +1,28 @@
+# Основное облако проекта
+resource "aws_vpc" "main_secure_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "Secure-Architecture-VPC"
+  }
+}
+
+# Публичный уровень (Web)
+resource "aws_subnet" "public_web" {
+  vpc_id                  = aws_vpc.main_secure_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = { Name = "Public-Web-Subnet" }
+}
+
+# Приватный уровень (Database) — сюда нет доступа из интернета
+resource "aws_subnet" "private_db" {
+  vpc_id            = aws_vpc.main_secure_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = { Name = "Private-DB-Subnet" }
+}
